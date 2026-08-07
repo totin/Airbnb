27f05aa2e0cc7054581a961841512d257fd2d734
totin <tomas.cittanti@philips.edu.ar>
2026-08-07 11:57:33 -0300

    Modelo propiedad

---
+++ backend/src/db/models/propiedad.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from database import Base
+
+class Propiedad(Base):
+    __tablename__ = "propiedades"
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String(150), nullable=False)
+    direccion = Column(String(200), nullable=False)
+    ciudad = Column(String(100), nullable=False)
+    precio_noche = Column(Float, nullable=False)
+    capacidad = Column(Integer, nullable=False)
+
+    anfitrion_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
