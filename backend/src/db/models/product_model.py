5ff61d10224034b285811656eced33c0c09741fc
totin <tomas.cittanti@philips.edu.ar>
2026-08-14 08:27:07 -0300

    Borro producto

--- backend/src/db/models/product_model.py
+++
@@ -1,9 +0,0 @@
-# TODO: definir el modelo Product (id, name, price, stock, created_at, etc.)
-# Seguí el patrón de user_model.py
-#
-# from sqlalchemy import Column, Integer, String, Numeric
-# from src.db.connection import Base
-#
-# class Product(Base):
-#     __tablename__ = "products"
-#     ...
