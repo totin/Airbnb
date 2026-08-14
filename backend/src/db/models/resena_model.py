e91fa9e7c44bbae66b1ed3772c9c0db9b28d4933
agos <agostina.cepeda@philips.edu.ar>
2026-08-14 08:29:22 -0300

    Modelo Reseña

---
+++ backend/src/db/models/resena_model.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, ForeignKey, Date, Text, CheckConstraint, func
+from database import Base
+
+class Resena(Base):
+    __tablename__ = "resena"
+    id = Column(Integer, primary_key=True, index=True)
+    reserva_id = Column(
+        Integer,
+        ForeignKey("reserva.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True
+    )
+    autor_id = Column(
+        Integer,
+        ForeignKey("usuario.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    puntaje = Column(
+        Integer,
+        nullable=False
+    )
+    comentario = Column(Text)
+    fecha = Column(
+        Date,
+        nullable=False,
+        server_default=func.current_date()
+    )
+    __table_args__ = (
+        CheckConstraint(
+            "puntaje BETWEEN 1 AND 5",
+            name="chk_puntaje"
+        ),
+    )
