69a528b72d4933f45888b7938739a827ba9c635b
ana camera <ana.camera@philips.edu.ar>
2026-08-14 10:42:41 -0300

    Borro router`s -auth -conductores -pasajero -user -product

--- backend/src/routers/product_router.py
+++
@@ -1,5 +0,0 @@
-# TODO: armar el APIRouter de productos siguiendo el patrón de user_router.py
-#
-# from fastapi import APIRouter, Depends
-# router = APIRouter(prefix="/products", tags=["products"])
-# ...
