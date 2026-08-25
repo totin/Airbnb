9fd60125dab4066c50cff1bdb7d5fff35482c57e
totin <tomas.cittanti@philips.edu.ar>
2026-08-25 13:51:44 -0300

    borro imagen_url, lo agrego como campo en propiedad

--- backend/src/db/models/propiedad.py
+++ backend/src/db/models/propiedad.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey
 from src.db.connection import Base
 
 class Propiedad(Base):
@@ -6,9 +6,8 @@ class Propiedad(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String(150), nullable=False)
-    direccion = Column(String(200), nullable=False)
-    ciudad = Column(String(100), nullable=False)
-    precio_noche = Column(Float, nullable=False)
-    capacidad = Column(Integer, nullable=False)
-
+    descripcion = Column(Text, nullable=True)
+    precio = Column(Float, nullable=False)
+    ubicacion = Column(String(200), nullable=True)
+    imagen_url = Column(String(500), nullable=True)
     anfitrion_id = Column(Integer, ForeignKey("usuario.id"), nullable=False)
