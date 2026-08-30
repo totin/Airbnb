782b203cd62e04598058437c877148d33463f43a
totin <tomas.cittanti@philips.edu.ar>
2026-08-30 00:51:53 -0300

    actualizo model propiedad para imagenes

--- backend/src/db/models/propiedad.py
+++ backend/src/db/models/propiedad.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
+from sqlalchemy.orm import relationship
 from src.db.connection import Base
 
 
@@ -12,3 +13,9 @@ class Propiedad(Base):
     precio_noche = Column(Numeric(10, 2), nullable=False)
     capacidad = Column(Integer, nullable=False)
     anfitrion_id = Column(Integer, ForeignKey("usuario.id"), nullable=False)
+
+    imagenes = relationship(
+        "PropiedadImagen",
+        cascade="all, delete-orphan",
+        order_by="PropiedadImagen.orden"
+    )
