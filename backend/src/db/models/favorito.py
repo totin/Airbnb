95943e80b4e3afe2b0b42c01205a25dbfbc2daf4
totin <tomas.cittanti@philips.edu.ar>
2026-08-30 01:03:18 -0300

    actualizo favorito.py

--- backend/src/db/models/favorito.py
+++ backend/src/db/models/favorito.py
@@ -5,7 +5,7 @@ from src.db.connection import Base
 class Favorito(Base):
     __tablename__ = "favoritos"
 
-    usuario_id = Column(Integer, ForeignKey("usuarios.id"), primary_key=True)
-    propiedad_id = Column(Integer, ForeignKey("propiedades.id"), primary_key=True)
+    usuario_id = Column(Integer, ForeignKey("usuario.id"), primary_key=True)
+    propiedad_id = Column(Integer, ForeignKey("propiedad.id"), primary_key=True)
 
     fecha = Column(DateTime, server_default=func.now())
