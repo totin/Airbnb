f541172a2579bb3cede742c2edfe94e9adc682e1
totin <tomas.cittanti@philips.edu.ar>
2026-08-30 19:07:46 -0300

    actualizo init.py para los nuevos models

--- backend/src/db/models/__init__.py
+++ backend/src/db/models/__init__.py
@@ -2,9 +2,12 @@ from .usuario import Usuario
 from .amenidad import Amenidad
 from .propiedad import Propiedad
 from .propiedad_amenidad import PropiedadAmenidad
+from .propiedad_imagen import PropiedadImagen
 from .reserva import Reserva
 from .resena import Resena
 from .favorito import Favorito
+from .saldo_horas import SaldoHoras
+from .transaccion_horas import TransaccionHoras
 
 __all__ = [
     "Usuario",
@@ -12,7 +15,10 @@ __all__ = [
     "Amenidad",
     "Propiedad",
     "PropiedadAmenidad",
+    "PropiedadImagen",
     "Reserva",
     "Resena",
     "Favorito",
+    "SaldoHoras",
+    "TransaccionHoras",
 ]
